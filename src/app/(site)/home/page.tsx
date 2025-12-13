23fd6c2065d327f860608901e208afc969764cc1
draftworks-repo <draftworks000@gmail.com>
2025-12-14 02:17:49 +0530

    home blak page added in components

---
+++ src/app/(site)/home/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <main
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Page Title</h1>
+      <p>This is a placeholder page.</p>
+    </main>
+  );
+}
