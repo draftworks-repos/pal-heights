f821a94fc32695b74031df4d07b0e860a38dfd97
draftworks-repo <draftworks000@gmail.com>
2025-12-15 00:44:37 +0530

    Homepage Section One added

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Sections/Hero";
+import SectionOne from "./Sections/Properties";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SectionOne />
     </>
   );
 }
