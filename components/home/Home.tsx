02509137a7bbe0a59f5c1312f26fa89a89548afb
draftworks-repo <draftworks000@gmail.com>
2025-12-15 02:12:49 +0530

    Home component updated with Section 2

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,11 +1,12 @@
 import Hero from "./Sections/Hero";
 import SectionOne from "./Sections/Properties";
-
+import SectionTwo from "./Sections/Blogs";
 export default function Home() {
   return (
     <>
       <Hero />
       <SectionOne />
+      <SectionTwo />
     </>
   );
 }
