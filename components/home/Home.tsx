642f2c529d59c55d77a4d4be986674a8fdd69565
draftworks-repo <draftworks000@gmail.com>
2025-12-15 16:14:23 +0530

    Section 3 added in Home component

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,12 +1,15 @@
 import Hero from "./Sections/Hero";
 import SectionOne from "./Sections/Properties";
 import SectionTwo from "./Sections/Blogs";
+import SectionThree from "./Sections/Reviews";
+
 export default function Home() {
   return (
     <>
       <Hero />
       <SectionOne />
       <SectionTwo />
+      <SectionThree />
     </>
   );
 }
