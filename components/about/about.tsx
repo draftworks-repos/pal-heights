aa0b0970ea973240603ec4023617c7b37e7e5d94
draftworks-repo <draftworks000@gmail.com>
2025-12-20 14:47:56 +0530

    About page.tsx added in about component

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -1,3 +1,17 @@
-export default function MyComponent() {
-  return <div>Hello</div>;
+import Hero from "./Sections/Hero";
+import SectionOne from "./Sections/Detail";
+import SectionTwo from "./Sections/Awards";
+import SectionThree from "../sharedSections/Reviews";
+import SectionFour from "./Sections/Connect";
+
+export default function About() {
+  return (
+    <>
+      <Hero />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+    </>
+  );
 }
