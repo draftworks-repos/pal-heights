c95928dd009c029f5658678cdac4e3dbec1347c5
draftworks-repo <draftworks000@gmail.com>
2025-12-20 14:57:32 +0530

    navbar tsx and css added in Layout

--- components/layout/Header/Navbar.tsx
+++ components/layout/Header/Navbar.tsx
@@ -1,36 +1,75 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const pathname = usePathname();
+  const isDarkBg = ["/about", "/discover", "/dining", "/banquets"].includes(
+    pathname
+  );
+
+  useEffect(() => {
+    const onScroll = () => setScrolled(window.scrollY > 50);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.menu}>
         <li>
-          <a href="/">Home</a>
+          <a href="/" data-cursor="hover" className={styles.a}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="/about">About Us</a>
+          <a href="/about" data-cursor="hover" className={styles.a}>
+            About Us
+          </a>
         </li>
         <li>
-          <a href="/discover">Discover Our Hotels</a>
+          <a href="/discover" data-cursor="hover" className={styles.a}>
+            Discover Our Hotels
+          </a>
         </li>
         <li>
-          <a href="/dining">Dining</a>
+          <a href="/dining" data-cursor="hover" className={styles.a}>
+            Dining
+          </a>
         </li>
         <li>
-          <a href="/banquets">Banquets</a>
+          <a href="/banquets" data-cursor="hover" className={styles.a}>
+            Banquets
+          </a>
         </li>
         <li>
-          <a href="/news-media">News & Media</a>
+          <a href="/news-media" data-cursor="hover" className={styles.a}>
+            News & Media
+          </a>
         </li>
         <li>
-          <a href="/careers">Careers</a>
+          <a href="/careers" data-cursor="hover" className={styles.a}>
+            Careers
+          </a>
         </li>
         <li>
-          <a href="/contact">Contact Us</a>
+          <a href="/contact" data-cursor="hover" className={styles.a}>
+            Contact Us
+          </a>
         </li>
       </ul>
 
-      <a href="/book-now" className={styles.bookNow} data-cursor="hover">
+      <a
+        href="/book-now"
+        className={`${styles.bookNow} ${isDarkBg ? styles.light : ""} ${
+          scrolled ? styles.scrolledButton : ""
+        }`}
+        data-cursor="hover"
+      >
         Book Now
       </a>
     </nav>
