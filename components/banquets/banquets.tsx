fce868afe95098a73eab90694d3cf293669d2372
draftworks-repo <draftworks000@gmail.com>
2025-12-20 14:50:19 +0530

    banquets.tsx added in banquets component

--- components/banquets/banquets.tsx
+++ components/banquets/banquets.tsx
@@ -1,3 +1,19 @@
-export default function MyComponent() {
-  return <div>Hello</div>;
+import Hero from "./Sections/Hero";
+import SectionOne from "./Sections/DiscoverRooms";
+import SectionTwo from "./Sections/DiscoverRoomsDark";
+import SectionThree from "./Sections/WeddingGallery";
+import SectionFour from "./Sections/CorporateGallery";
+import SectionFive from "./Sections/InstaPosts";
+
+export default function Banquets() {
+  return (
+    <>
+      <Hero />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+    </>
+  );
 }
